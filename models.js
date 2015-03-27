4a3cb73d18c387c9db1af4d9a7937df749190ac4
dashersw <armagan@amcalar.com>
2015-03-27 04:21:08 +0200

    Add product model

--- models.js
+++ models.js
@@ -2,10 +2,17 @@ var orm = require('orm');
 var db = orm.connect('sqlite://' + __dirname + '/db.sqlite');
 db.settings.set('instance.cache', false);
 
+var Product = db.define('product', {
+    name: String,
+    price: Number,
+    stock: Number
+});
+
 function init(callback) {
     db.sync(callback);
 }
 
 module.exports = {
+    Product: Product,
     init: init
 };
