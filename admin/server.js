e9f95830f273d61134a877f14326b15c2cd0dcf3
dashersw <armagan@amcalar.com>
2015-03-27 04:40:09 +0200

    Implement product add / list functionality to admin panel

--- admin/server.js
+++ admin/server.js
@@ -1,7 +1,8 @@
 var app = require('express')(),
     bodyParser = require('body-parser'),
     server = require('http').Server(app),
-    io = require('socket.io')(server);
+    io = require('socket.io')(server),
+    cote = require('cote');
 
 app.use(bodyParser.json());
 
@@ -9,4 +10,21 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/product', function(req, res) {
+    productRequester.send({ type: 'list' }, function(err, products) {
+        res.send(products);
+    });
+});
+
+app.post('/product', function(req, res) {
+    productRequester.send({ type: 'create', product: req.body.product }, function(err, product) {
+        res.send(product);
+    });
+});
+
+
+var productRequester = new cote.Requester({
+    name: 'admin product requester',
+    namespace: 'product'
+});
 server.listen(5000);
--- services/product-service.js
+++ services/product-service.js
@@ -5,3 +5,12 @@ var productResponder = new cote.Responder({
     name: 'product responder',
     namespace: 'product'
 });
+
+productResponder.on('list', function(req, cb) {
+    var query = req.query || {};
+    models.Product.find(query, cb);
+});
+
+productResponder.on('create', function(req, cb) {
+    models.Product.create(req.product, cb);
+});
