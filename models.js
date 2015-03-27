30dc5b3ac2ab66399a3f22a1f7b843d8da401ebc
dashersw <armagan@amcalar.com>
2015-03-27 04:12:33 +0200

    Don't cache model instances

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 var orm = require('orm');
 var db = orm.connect('sqlite://' + __dirname + '/db.sqlite');
+db.settings.set('instance.cache', false);
 
 function init(callback) {
     db.sync(callback);
