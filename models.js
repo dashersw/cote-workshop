aceeb92a1c71a2bdae0f1ebfce50c2391a20bbe2
dashersw <armagan@amcalar.com>
2015-03-27 04:12:16 +0200

    Use correct directory for sqlite database

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 var orm = require('orm');
-var db = orm.connect('sqlite://db.sqlite');
+var db = orm.connect('sqlite://' + __dirname + '/db.sqlite');
 
 function init(callback) {
     db.sync(callback);
