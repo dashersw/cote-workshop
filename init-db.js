e8573762eb64f86f66e779c1a551d821324fdaf6
dashersw <armagan@amcalar.com>
2016-02-02 22:00:12 +0100

    Add fixture data to the db for a faster set up

--- init-db.js
+++ init-db.js
@@ -1,6 +1,29 @@
 var models = require('./models');
 
+var products = [
+    {
+        name: 'Fancy dish',
+        price: 1,
+        stock: 20
+    },
+    {
+        name: 'Healthy food',
+        price: 2,
+        stock: 20
+    },
+    {
+        name: 'Feast for kings',
+        price: 3,
+        stock: 20
+    }
+];
+
 models.init(function(err) {
     if (err) return console.log(err);
-});
 
+    products.forEach(function(product) {
+        models.Product.create(product, function(err, product) {
+            console.log('Initialized', product.name);
+        });
+    });
+});
